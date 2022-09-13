66ee4c1de4065234c691f48aba3d36c592da467c
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-13 16:59:26 -0700

    0 task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,1 @@
-#include <unistd.h>
-
-/**
- * _putchar- writes a character to standard output
- * @c: The character to print
- * Return: Always 1 (Sucess)
- *
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
