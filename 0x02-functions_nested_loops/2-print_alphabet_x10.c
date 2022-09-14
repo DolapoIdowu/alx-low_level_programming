40b22f2e042bd633ee811e1d643eaeed04129adc
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-13 18:00:54 -0700

    2 task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- print 10x alphabelt
+ */
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	char letter = 'a';
+
+	while (num < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		num++;
+	}
+}
