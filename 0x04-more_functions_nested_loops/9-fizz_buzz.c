7811b7b59a02c33c6e31f7d11f300bf028a409b0
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-15 09:35:24 -0700

    9 task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,22 +18,20 @@ void main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 != 0))
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((num % 5 == 0) && (num % 3 != 0))
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if (num % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d ", num);
 		}
-
-		printf(" ");
 		num++;
 	}
 	printf("\n");
