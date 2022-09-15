94945e847bd6855b1375473d30ec25e257245508
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-15 01:53:06 -0700

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul- multiply two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	int  result;
+
+	result = a * b;
+
+	return (result);
+}
