e21ccec50fc13b77047a2b96566eb9311e40d741
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-19 14:45:26 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen-returns the length of a string.
+ * @s: string whom length is to be determined
+ *
+ * Return: Length oof string.
+ */
+int _strlen(char *s)
+{
+	int num = 0;
+
+	while (s[num] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
