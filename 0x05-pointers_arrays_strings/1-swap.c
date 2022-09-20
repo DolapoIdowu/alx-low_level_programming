7faba55352e294a6c1ca73d9aae11025c52b30c2
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-19 21:07:38 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
+#include "main.h"
 /**
- * -a function that returns the length of a string.
- * @a: integer to be swap.
- * @b: integer to be swapped.
- * Return: nothing.
+ * swap_int -a function that swaps the values of two integers.
+ * @a: integer to be swapped.
+ * @b: integer ro be swpped.
+ *
+ * Rwturn: nothing.
  */
 void swap_int(int *a, int *b)
 {
 	int num;
+
 	num = *a;
-	a* = b*;
+	*a = *b;
 	*b = num;
 }
+
