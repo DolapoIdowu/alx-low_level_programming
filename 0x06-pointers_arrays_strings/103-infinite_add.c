2ab0257633e575c6c93d8b985000709fbedf94d1
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-22 11:23:21 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * infinite_add- a function that adds two numbers.
+ * @n1: first number.
+ * @n2: second number.
+ * @r: the buffer that the function will use to store the result.
+ * @size_r: the buddwr size.
+ * Return: a pointer to the result
+ */
+
