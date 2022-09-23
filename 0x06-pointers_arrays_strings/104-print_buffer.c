6cfae90df4b936e5103808f400f1a3a34a12b2da
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-22 21:17:16 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,32 +9,34 @@
  */
 void print_buffer(char *b, int size)
 {
-	 int j, k, l;
+	 int j, k, l;
 
-	if (size <= 0)
+	if (size <= 0)
+	{
 		printf("\n");
+	}
 	else
 	{
-		for (j = 0; j < size; j += 10)
+		for (j = 0; j < size; j += 10)
 		{
-			printf("%.8x:", j);
-			 for (k = j; k < j + 10; k++)
+			printf("%.8x:", j);
+			 for (k = j; k < j + 10; k++)
 			{
-				if (k % 2 == 0)
+				if (k %  2 == 0)
 					printf(" ");
 				if (k < size)
-					printf("%.2x", *(b + k));
+					printf("%.2x", *(b + k));
 				else
 					printf(" ");
 			}
-			for (l = j; l < j + 10; l++)
+			for (l = j; l < j + 10; l++)
 			{
-				if (l >= size)
+				if (l >= size)
 					break;
-				if (*(b + l) < 32 || *(b + l) > 126)
-					printf("%c", '.');
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
 				else
-					printf("%c", *(b + l));
+					printf("%c", *(b + l));
 			}
 			printf(" ");
 		}
