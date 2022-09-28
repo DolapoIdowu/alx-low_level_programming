c4985a7e878f5d34083508f9ebd5b1229e3c3911
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-28 12:26:33 -0700

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper_recursion(int num, int root);
+int is_prime_number(int n);
+int prime_number_helper(int num, int factor);
