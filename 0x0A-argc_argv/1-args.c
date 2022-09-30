d4b8380e099aa58e539bd8d348209ccf1843d92c
Pizzle power electrical <dollipissul1@gmail.com>
2022-09-30 07:15:42 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- prints the number of arguments aside from program name.
+ * @argc: number of argument.
+ * @argv: arrays of string argument.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int count, i;
+
+	count = 1;
+	for (i = 1; i < argc; i++)
+		count += 1;
+	printf("%d", count);
+	return (0);
+}
