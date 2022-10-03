5f52e691dde36f55a952bc1b00a2bbf16901595e
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-03 16:28:41 -0700

    The second commit i am making tonight.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup- returns a pointer to a newly allocated space
+ *	in memory, which contains a copy of the
+ *	string given as a parameter.
+ * @str: string to be duplicated.
+ * Return: NULL if str = NULL,
+ *	a pointer to the duplicated string,
+ *	NULL if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	char *new_string;
+	unsigned int len, i;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		++len;
+	}
+	if (str == NULL)
+		return (NULL);
+	new_string = malloc(len * sizeof(char));
+	for (i = 0; i < len; i++)
+	{
+		new_string[i] = str[i];
+	}
+	str[i] = '\0';
+	return (new_string);
+}
