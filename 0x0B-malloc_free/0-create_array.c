71d07dd17beffb8915d28fb18306809b1c12e512
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-03 15:43:44 -0700

    The very first commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,14 +20,14 @@ char *create_array(unsigned int size, char c)
 
 	ptr = malloc(size * sizeof(char));
 	if (ptr == 0)
-		return (NULL);
-	else
 	{
-		for (i = 0; i < size; i++)
-		{
-			ptr[i] = c;
-		}
-
-		return (ptr);
+		return (NULL);
 	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+
 }
