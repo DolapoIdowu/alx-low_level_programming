58d7bf764e24eb8c76aa4b7b9e1c9b1553a9c9d0
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-04 05:16:58 -0700

    The third commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *new_string;
-	unsigned int len1, len2, i, j;
+	unsigned int len1, len2, i, j, len;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -26,18 +26,16 @@ char *str_concat(char *s1, char *s2)
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
-	j = 0;
-	while (s2[j] != '\0' && j < len2)
-	{
-		s1[len1] = s2[j];
-		j++;
-		len1++;
-	}
-	s1[len1] =  '\0';
-	new_string = malloc(len1 * sizeof(char));
+	len = len1 + len2 + 1;
+	new_string = malloc(len * sizeof(char));
 	if (new_string == 0)
 		return (NULL);
-	for (i = 0; i < len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		new_string[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		new_string[i] = s2[j];
+		i++;
+	}
 	return (new_string);
 }
