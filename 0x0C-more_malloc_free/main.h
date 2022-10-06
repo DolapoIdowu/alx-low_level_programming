be3aa33ef1a9f199e30a6943b9c1d81493725d0c
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-06 12:07:02 -0700

    sixth commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,10 @@ int *array_range(int min, int max);
 int _strlen(char *s);
 char *copychar(char *s1, char c, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+int _putchar(char c);
+int _atoi_digit(char x);
+int _isNumber(char *argv);
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena);
+void print_array(char *a, int nb);
 
 #endif
