2d903a497654452be98a846a3ca6fcabcfb460f9
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-06 09:58:06 -0700

    Third commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _strlen(char *s);
+char *copychar(char *s1, char c, unsigned int n);
+
 
 #endif
