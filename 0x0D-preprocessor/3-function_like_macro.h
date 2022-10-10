58b9310404e8098a40f8f7f7ab06ab344fed9a49
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-10 12:09:58 -0700

    Fourth commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef ABS(x)
+#ifndef FUNCITION_LIKE_MACRO_H
+#define FUNCITION_LIKE_MACRO_H
 
-#define ABS(x) ((x >= 0) ? (x) : (-1 * x))
+#define ABS(x) ((x >= 0) ? (x) : (-x))
 
-#endif /*#ifndef ABS(x)*/
+#endif /*#ifndef FUNCITION_LIKE_MACRO_H*/
