9265c39926c7591d735e5e42ab9e826bff403330
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-11 05:24:48 -0700

    fifth commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog- create new  dog.
+ * @name: name of new dog.
+ * @age: age of new dog
+ * @owner: owner of new dog.
+ * Return: New dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	int i = 0, j = 0, k;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = malloc(i * sizeof(new_dog->name));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		new_dog->name[k] = name[k];
+	new_dog->age = age;
+	new_dog->owner = malloc(i * sizeof(new_dog->owner));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		new_dog->owner[k] = name[k];
+	return (new_dog);
+}
