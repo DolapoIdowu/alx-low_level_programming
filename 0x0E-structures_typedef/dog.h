4ba0ebf48111c65c99934d8e477c10a9bb3b858b
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-11 02:03:48 -0700

    First commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog- structure for a dog.
+ * @name: name of Dog.
+ * @age: dog age.
+ * @owner: dog owner.
+ * Description: This struct gives us information of a dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
