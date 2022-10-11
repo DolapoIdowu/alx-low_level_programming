f0f9a1ce13381b50ef8c38174740829ceafe8817
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-11 03:19:17 -0700

    Third commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 
 		printf("Age: %f\n", d->age);
 
-		if (d->owner != NULL)
+		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nill)\n");
