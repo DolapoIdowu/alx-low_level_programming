fda689d2a3b84c93ac15ad2056f8f86be93f263e
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-11 14:05:59 -0700

    Just correcting

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog- structure for a dog.
@@ -20,4 +20,4 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*#ifndef DOG_H*/
+#endif /*#ifndef _DOG_H*/
