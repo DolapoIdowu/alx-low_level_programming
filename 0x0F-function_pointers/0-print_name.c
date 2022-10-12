5814172aa76e1e0a7ca9c45856040f66ff4be1ba
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-12 08:02:20 -0700

    first commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+
 /**
  * print_name-  prints a name.
  * @name: name to be printed.
  * @f: funtion pointer f.
  * Return: Nothing.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
