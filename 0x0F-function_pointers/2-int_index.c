b57dc6e7bf4606f3ece6fe5feb8c40df5fa0ee5b
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-12 09:06:21 -0700

    Third commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,15 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
-	if (i == size)
-		return (-1);
+	return (-1);
 }
