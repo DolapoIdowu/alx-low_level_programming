6ca3217b69a8124e62ce7c2d4af80b793e55eca2
Pizzle power electrical <dollipissul1@gmail.com>
2022-10-13 09:51:51 -0700

    first commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
 	va_list args;
 	unsigned int i;
 	int u_arg, sum = 0;
 
+	if (n == 0)
+		return (0);
+
 	va_start(args, n);
+
 	i = 0;
 	while (i < n)
 	{
